dc868c383ee38af4ae300c6e874d2342f4dd739b
IvethCastro <iveth.castro@udla.edu.ec>
2025-12-21 22:01:04 -0500

    Refactor structs and add date handling in funciones.h

--- funciones.h
+++ funciones.h
@@ -11,25 +11,46 @@ typedef struct {
     char tipo[20];   
     int usado;       
     float precio;
-    int disponible;  
+    int cantidad;  
 } Vehiculo;
+
+typedef struct {
+    int dia;
+    int mes;
+    int anio;
+} Fecha;
 
 typedef struct {
     Cliente cliente;
     Vehiculo vehiculo;
+    Fecha fechaVenta;
+    float totalPagado;
 } Venta;
 
+
+/* Utilidades */
 void leerCadena(char *cadena, int n);
 int leerEnteroConRango(int inicio, int fin);
-float leerFlotanteConRango(float inicio, float fin);
 int soloLetras(char *cadena);
+
+/* Menús */
 int menu();
-void registrarCliente();
+int menuMarcas();
 int seleccionarEstado();
-int obtenerTiposPorEstado(int usado, char tipos[][20]);
-void listarVehiculosPorEstadoYTipo(int usado, char *tipo);
+
+/* Clientes */
+void registrarCliente();
+
+/* Vehículos */
 void buscarVehiculos();
+void ingresarCantidadVehiculo();
 void cargarVehiculosIniciales();
+
+/* Ventas */
 void venderVehiculo();
 void listarVentas();
+
+/* Fecha */
+Fecha ingresarFecha();
+
 
